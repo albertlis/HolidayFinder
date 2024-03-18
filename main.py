d5815e6367b238fd18c3017f6c51a4b45a9678d0
Albert Lis <albert.lis@zebra.com>
2024-03-18 14:19:46 +0100

    Add F4F and cache

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import logging
+import pickle
 import re
 import time
+from pathlib import Path
 
 import feedparser
 import requests
 import schedule
 import yagmail
 import yaml
+from bs4 import BeautifulSoup
 from easydict import EasyDict
 from feedparser import FeedParserDict
 from jinja2 import Template
 from selenium import webdriver
+from selenium.common import TimeoutException, NoSuchElementException
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
@@ -83,9 +87,31 @@ def render_html(offers: list[EasyDict]) -> str:
 
 
 def get_lastminuter_entries(rss_url: str) -> list[FeedParserDict]:
-    rss = requests.get(rss_url)#, verify=False)
+    rss = requests.get(rss_url, verify=VERIFY_SSL)
     feed = feedparser.parse(rss.text)
     return feed.entries
+
+
+def get_lastminuter_offer_category(url: str) -> str:
+    response = requests.get(url, verify=VERIFY_SSL)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    span = soup.find('span', {'class': 'deals__valid_until'})
+    text = ''
+    if span:
+        text = span.get_text().replace('Termin ', '')
+
+    section = soup.find('section', {'id': 'offer_page'})
+    if not section:
+        return text
+
+    spans = section.find_all('span')  # find all span elements
+    for span in spans:
+        img = span.find('img', alt='clock icon')
+        if not img:
+            img = span.find('img', alt='boarding icon')
+        if img:
+            text = f'{text} {span.get_text()}'
+    return text
 
 
 def lastminuter_to_offer(feed: FeedParserDict) -> EasyDict:
@@ -95,7 +121,67 @@ def lastminuter_to_offer(feed: FeedParserDict) -> EasyDict:
     if price:
         price = price.group()
         title = re.sub(price, '', title)
-    return EasyDict(link=feed.link, title=title, price_str=price, category='')
+    return EasyDict(link=feed.link, title=title, price_str=price, category=get_lastminuter_offer_category(feed.link))
+
+
+def get_fly4free_divs(driver: webdriver.Chrome, category: str, is_first_call: bool = False) -> list[WebElement]:
+    if category == 'first-minute':
+        driver.get(f'https://www.fly4free.pl/tag/first-minute/')
+    else:
+        driver.get(f'https://www.fly4free.pl/tanie-loty/{category}/')
+    if is_first_call:
+        try:
+            button = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, "//button[normalize-space()='ZGADZAM SIĘ']"))
+            )
+            # Accept cookies
+            button.click()
+        except TimeoutException:
+            pass
+
+    offers_div = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME, 'row-equal'))
+    )
+    offers_divs = offers_div.find_elements(By.XPATH, "./div[contains(@class, 'col-xs-12')]")
+
+    filtered_offers_divs = []
+    for offer_div in offers_divs:
+        # Check if the div has a direct child div with the class 'item--soldout'
+        soldout_divs = offer_div.find_elements(By.XPATH, "./div[contains(@class, 'item--soldout')]")
+
+        # If it doesn't have such a child div, add it to the filtered list
+        if not soldout_divs:
+            filtered_offers_divs.append(offer_div)
+    return filtered_offers_divs
+
+
+def get_fly4free_category_offers(offers_divs: list[WebElement], category: str) -> list[EasyDict]:
+    offers = []
+    for offer_div in offers_divs:
+        try:
+            price_div = offer_div.find_element(By.CLASS_NAME, 'item__price')
+        except NoSuchElementException:
+            continue
+
+        price_text = price_div.text
+        if category == 'loty':
+            price_int = int(re.search(r'\d+', price_text).group())
+            if price_int > 1000:
+                continue
+        if category in {'wczasy', 'majowka', 'first-minute'}:
+            category = 'wczasy'
+
+        title = offer_div.find_element(By.CLASS_NAME, 'item__title')
+        a_tag = title.find_element(By.TAG_NAME, 'a')
+        href = a_tag.get_attribute('href')
+        offer = EasyDict(
+            category=category,
+            price_str=price_text,
+            title=a_tag.text,
+            link=href
+        )
+        offers.append(offer)
+    return offers
 
 
 def send_mail(html_content: str) -> None:
@@ -106,17 +192,32 @@ def send_mail(html_content: str) -> None:
     yag.send(to=secrets.dst_mail, subject=email_subject, contents=(html_content, 'text/html'))
 
 
+def load_from_cache(previous_piraci_offers: list, previous_lastminuter_offers: list, previous_fly4free_offers: list):
+    cache = Path('previous_offers.pkl')
+    if cache.exists():
+        with cache.open('rb') as f:
+            unpickled_cache = pickle.load(f)
+        previous_piraci_offers.extend(unpickled_cache['previous_piraci_offers'])
+        previous_lastminuter_offers.extend(unpickled_cache['previous_lastminuter_offers'])
+        previous_fly4free_offers.extend(unpickled_cache['previous_fly4free_offers'])
+
+
 LASTMINUTER_RSS_URL = 'https://www.lastminuter.pl/feed'
 
 previous_lastminuter_offers = []
 previous_piraci_offers = []
+previous_fly4free_offers = []
 
 
 def main():
-    global previous_piraci_offers, previous_lastminuter_offers
+    global previous_piraci_offers, previous_lastminuter_offers, previous_fly4free_offers
+
+    load_from_cache(previous_piraci_offers, previous_lastminuter_offers, previous_fly4free_offers)
+
     lastminuter_entries = get_lastminuter_entries(LASTMINUTER_RSS_URL)
     offers = [lastminuter_to_offer(e) for e in lastminuter_entries]
     offers = [offer for offer in offers if offer not in previous_lastminuter_offers]
+    previous_lastminuter_offers = [*previous_lastminuter_offers, *offers][-300:]
     driver = get_driver()
     divs = get_wakacyjni_piraci_divs(driver)
 
@@ -124,7 +225,19 @@ def main():
     piraci_offers = create_wakacyjni_piraci_offers(filtered_divs)
     piraci_offers = [offer for offer in piraci_offers if offer not in previous_piraci_offers]
     offers.extend(piraci_offers)
-    previous_piraci_offers = piraci_offers
+    previous_piraci_offers = [*previous_piraci_offers, *piraci_offers][-300:]
+
+    categories = ('loty', 'wczasy', 'weekend')
+    all_fly4free_offers = []
+    for i, cat in enumerate(categories):
+        filtered_offers_divs = get_fly4free_divs(driver, cat, is_first_call=(i == 0))
+        fly4free_offers = get_fly4free_category_offers(filtered_offers_divs, cat)
+        for offer in fly4free_offers:
+            if (offer not in all_fly4free_offers) and (offer not in previous_fly4free_offers):
+                all_fly4free_offers.append(offer)
+
+    offers.extend(all_fly4free_offers)
+    previous_fly4free_offers = [*previous_fly4free_offers, *all_fly4free_offers][-200:]
 
     driver.close()
 
@@ -132,16 +245,28 @@ def main():
     # with open('holidays.html', 'w', encoding='utf-8') as f:
     #     f.write(html_content)
 
+    with open('previous_offers.pkl', 'wb') as f:
+        pickle.dump(
+            dict(
+                previous_fly4free_offers=previous_fly4free_offers,
+                previous_piraci_offers=previous_piraci_offers,
+                previous_lastminuter_offers=previous_lastminuter_offers
+            ), f, pickle.HIGHEST_PROTOCOL
+        )
+
     send_mail(html_content)
 
 
+VERIFY_SSL = True
 if __name__ == '__main__':
     logging.basicConfig(filename='error.log', level=logging.ERROR, format='%(asctime)s - %(levelname)s - %(message)s')
-    schedule.every().day.at("13:00").do(main)
+    schedule.every().day.at("12:00").do(main)
     while True:
         try:
             schedule.run_pending()
         except Exception as e:
             logging.exception("An error occurred:")
+            print(e)
+            time.sleep(60*60)
         time.sleep(1)
     # main()
